f787bdcf01f8449ef72d1e1068ff2d87cdd65edb
zeng qing <50618591@qq.com>
2024-11-26 21:14:15 +0800

    类静态对象

--- allchapter/2chapter/src/2-28.ts
+++ allchapter/2chapter/src/2-28.ts
@@ -5,6 +5,7 @@ class People{
    par1:string="dddd"
    par2?:string
    par3!:string
+   static count:number
    constructor(_name:string,_age:number,_addr:string){
         this.name=_name
         this.age=_age
@@ -16,5 +17,7 @@ class People{
 }
 let p=new People("zq",22,"sz");
 p.doEat()
+People.count=1
+
 
 export{}
