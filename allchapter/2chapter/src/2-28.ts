bd00c2541be65089cab37e536292ca1a323de3d1
zeng qing <50618591@qq.com>
2024-11-26 21:22:25 +0800

    类静态变量标识new了多少个对象

--- allchapter/2chapter/src/2-28.ts
+++ allchapter/2chapter/src/2-28.ts
@@ -5,11 +5,12 @@ class People{
    par1:string="dddd"
    par2?:string
    par3!:string
-   static count:number
+   static count:number=0
    constructor(_name:string,_age:number,_addr:string){
         this.name=_name
         this.age=_age
         this.addr=_addr
+        People.count++
    }
    doEat(){
 
@@ -17,7 +18,10 @@ class People{
 }
 let p=new People("zq",22,"sz");
 p.doEat()
-People.count=1
 
+let p2=new People("z2",33,"dsfs")
+let p3=new People("z3",33,"dsfs")
+
+console.log("peo:",People.count)
 
 export{}
